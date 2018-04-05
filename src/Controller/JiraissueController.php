9b9536c596b454cf70cd23785323037854fc71f3
John Doe <probertorios@gmail.com>
2018-04-05 16:37:36 -0400

    cover

--- src/Controller/JiraissueController.php
+++ src/Controller/JiraissueController.php
@@ -16,7 +16,7 @@ class JiraissueController extends AppController
         // Default to first issue on task list if no issue key passed in
         if (null == $issueKey) {
           // Build API call
-          $apiStart = 'https://itops.chattanooga.gov/rest/api/latest/search';
+          $apiStart = 'hXX//XXXXXXXXX/rest/api/latest/search';
           $apiMax = '?maxResults=100';
           $apiJql = '&jql=issuetype%20%3D%20%22Tier%20Two%22%20AND%20resolution%20%3D%20Unresolved%20AND%20assignee%20in%20(dsmith%2C%20dtiller%2C%20EMPTY)';
           $apiSort = '%20order%20by%20assignee';
@@ -26,7 +26,7 @@ class JiraissueController extends AppController
           // Send API call and receive response
           $http = new Client();
           $response = $http->get($fullApi, [], [
-                          'auth' => ['username' => 'ITOPSAPI', 'password' => '?Ch@ttG0v17!']
+                          'auth' => ['username' => 'XXXXXXX', 'password' => 'XXXXXXXXX']
                       ]);
           $json = $response->json;   //Returns formatted as an array
           $issues = $json['issues']; // Skip the first couple of fields, start with the recurring Issues array
